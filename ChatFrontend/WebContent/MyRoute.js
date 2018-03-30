931a2d80c7388fd9d111b9988e79137fdc830676
Lungdsuo Mozhui <33083654+mozhulungdsuo@users.noreply.github.com>
2018-03-30 09:43:18 +0530

    made changes

--- ChatFrontend/WebContent/MyRoute.js
+++ ChatFrontend/WebContent/MyRoute.js
@@ -5,17 +5,20 @@ myApp.config(function($routeProvider) {
 	$routeProvider.when("/", {
 		templateUrl : "/index.jsp"
 	})
+	.when("/Home",{
+		templateUrl :"/index.jsp"
+	})
 	.when("/Login", {
 		templateUrl : "views/Login.html"
 	})
 	.when("/Register", {
 		templateUrl : "views/Register.html"
 	})
-	.when("/About Us", {
-		templateUrl : "/AboutUs.htm"
+	.when("/AboutUs", {
+		templateUrl : "views/AboutUs.html"
 	})
-	.when("/Contact Us", {
-		templateUrl : "/ContactUs.htm"
+	.when("/ContactUs", {
+		templateUrl : "views/ContactUs.html"
 	})
 
 });
