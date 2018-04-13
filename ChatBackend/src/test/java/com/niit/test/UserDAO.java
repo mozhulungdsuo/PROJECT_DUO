6836571f8fb3985082768065f81c023428fa924a
Lungdsuo Mozhui <33083654+mozhulungdsuo@users.noreply.github.com>
2018-04-13 10:35:38 +0530

    made changes

---
+++ ChatBackend/src/test/java/com/niit/test/UserDAO.java
@@ -0,0 +1,34 @@
+package com.niit.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.controller.DBConfig;
+
+public class UserDAO {
+	private static DBConfig config;
+	@Autowired
+	private static UserDAO userDAO;
+	
+	@BeforeClass	
+	public static void setup()
+	{
+		  config = new DBConfig();
+	
+	     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+	      context.scan("com.niit");
+	     context.refresh();
+	     
+	
+	     userDAO=(UserDAO) context.getBean("userDAO");
+	}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
